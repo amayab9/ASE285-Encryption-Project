b6e7f06437fc806afe3c2fd47f8c126d3d722dfd
Amaya Bryant <100623332+amayab9@users.noreply.github.com>
2024-04-05 13:58:33 -0400

    config updated

--- util/config.js
+++ util/config.js
@@ -1,8 +1,3 @@
-// util/config.js
-//**For some reason the process.env.MONGO_URI is not properly connecting to the DB and using the connection string
-//inside the mongoose.connect is the only solution at the moment
-//unable to resolve so far, but looking for solutions
-
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
@@ -13,7 +8,8 @@ const connectionString = process.env.MONGO_URI
 const connectToDatabase = async () => {
     try {
         console.log('MONGO_URI:', process.env.MONGO_URI);
-        await mongoose.connect("mongodb+srv://bryanta21:zFTXDUdWEJv4aMGK@cluster0.qdvgn0b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+        //use connection string when .env decides to not work
+        await mongoose.connect("mongodb+srv://bryanta21:BEZOZ62Zfu4ueAtu@cluster0.qdvgn0b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
         //await mongoose.connect(connectionString);
 
         console.log('Connected to MongoDB');
